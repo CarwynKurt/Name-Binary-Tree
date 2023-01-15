7bdbaee3dee4b6c0a826aec31a3d336a142d25a8
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-15 19:01:44 +0800

    Post Order Traversal

--- NameBinaryTree.py
+++ NameBinaryTree.py
@@ -36,11 +36,23 @@ class BinarySearchTreeNode:
 
         return elements
 
-# Post Order Traversal
+    # Post Order Traversal
+    def post_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.post_order_traversal()
+        if self.right:
+            elements += self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
 # Pre Order Traversal
 # Search Option
+# Find Max
 # Find Min
 # Delete Function
 # Build Binary Tree
 # Binary Tree Creation
-
