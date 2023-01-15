1e6ec62af0e00452cd29a7c244a59692bc9b9f18
CarwynKurt <carwynkurtarimado@gmail.com>
2023-01-15 18:46:29 +0800

    Binary Class

---
+++ NameBinaryTree.py
@@ -0,0 +1,17 @@
+# Binary Class
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+# Create data for binary tree
+# In Order Traversal
+# Post Order Traversal
+# Pre Order Traversal
+# Search Option
+# Find Max
+# Find Min
+# Delete Function
+# Build Binary Tree
+# Binary Tree Creation
